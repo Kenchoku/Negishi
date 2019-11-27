b7071432fad90b2677a9dfc280116233df3f7733
t-negishi <t-negishi@i-enter.co.jp>
2019-11-27 15:57:19 +0900

    for

--- Negi.sh
+++ Negi.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-diff $1 $2 >/dev/null 2>&1
-EXIT_STATUS=$?
-if [ $EXIT_STATUS -eq 0 ]  
-then
-    echo "<$1><$2>  一致"
+it=" abcd day 1234 "
+for VAR in $it
+do
+    echo $it
+done
 
-elif [ $EXIT_STATUS -eq 1 ]
-then
-    echo "<$1><$2>  差異"
 
-else
-    echo "<$1><$2>  エラー"
 
-fi
+for VAR in AB " CD " EF 'GH IJ'.
+do
+    echo $VAR
+done
