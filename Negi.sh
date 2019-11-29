f46eeffb9e0fd5c5fa1f6dc3aeb43a20a66e7fc6
t-negishi <t-negishi@i-enter.co.jp>
2019-11-29 09:54:36 +0900

    while  nest

--- Negi.sh
+++ Negi.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-while read COL1 COL2
+NUM_1=999
+while [ $NUM_1 -gt 700 ]
 do
-    echo $COL1
-    echo $COL2
-done < file01 > file02
+    NUM_2=77
+    while [ $NUM_2 -le 99 ]
+    do
+         echo "\$NUM_1 * \$NUM_2 = <$NUM_1 * $NUM_2> = `expr $NUM_1 \* $NUM_2`"
+         NUM_2=`expr $NUM_2 + 11`
+    done
+    NUM_1=`expr $NUM_1 - 111`
+done
