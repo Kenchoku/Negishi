3d17be8cbf61853dcc70db85014b88afe0af3fe2
t-negishi <t-negishi@i-enter.co.jp>
2019-11-29 10:37:51 +0900

    case

--- Negi.sh
+++ Negi.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-NUM_1=999
-while [ $NUM_1 -gt 700 ]
-do
-    NUM_2=77
-    while [ $NUM_2 -le 99 ]
-    do
-         echo "\$NUM_1 * \$NUM_2 = <$NUM_1 * $NUM_2> = `expr $NUM_1 \* $NUM_2`"
-         NUM_2=`expr $NUM_2 + 11`
-    done
-    NUM_1=`expr $NUM_1 - 111`
-done
+case $1 in
+    [a-z]*) echo "\$1 starts lower-case       letter.";;
+    [A-Z]*) echo "\$1 starts upper-case       letter.";;
+    [0-9]*) echo "\$1 starts nameric          letter.";;
+         *) echo "\$1 starts non-alphanumeric letter.";;
+esac
+    
